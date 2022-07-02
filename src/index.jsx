7601ce5de3c9974c7c45b6fad27a37cbc2f586d2
Sofyan Egi <sofyanegil@gmail.com>
2022-07-02 09:31:36 +0700

    build: set global style

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,11 @@
 /* eslint-disable comma-dangle */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import NotesApp from './NotesApp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <NotesApp />
   </React.StrictMode>
 );
