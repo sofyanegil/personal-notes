10c0e9bd1167350fe45fbdd3b6b90ba43b38c1ca
Sofyan Egi <sofyanegil@gmail.com>
2022-07-02 09:32:19 +0700

    build: header and styling

---
+++ src/components/NotesHeader.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../styles/header.css';
+
+export default function NotesHeader() {
+  return (
+    <header>
+      <img src="/icons/logo.svg" alt="logo" width="40" />
+      <h1 className="notes-header">Notes Apps</h1>
+    </header>
+  );
+}
